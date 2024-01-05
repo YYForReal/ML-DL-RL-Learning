7769980a313d9b4dc4fd5a03380784ec9ec9f93a
yy <2572082773@qq.com>
2024-01-05 22:43:40 +0800

    win 更新模型环境及参数设置

--- RL-Learning/GYM/config.py
+++ RL-Learning/GYM/config.py
@@ -1,4 +1,4 @@
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 MAX_EPISODES = 101
 MAX_STEPS = 10000
 seed = 12345
@@ -7,6 +7,7 @@ lr_actor = 0.001
 lr_critic = 0.005
 gamma = 0.99
 hidden_dim = 128
+env_name = "ALE/DemonAttack-v5"
 
 hyperparameters = {
     "MAX_EPISODES": MAX_EPISODES,
@@ -15,5 +16,6 @@ hyperparameters = {
     "lr_critic": lr_critic,
     "gamma": gamma,
     "hidden_dim": hidden_dim,
-    "BATCH_SIZE":BATCH_SIZE
+    "BATCH_SIZE":BATCH_SIZE,
+    "env_name":env_name
 }
