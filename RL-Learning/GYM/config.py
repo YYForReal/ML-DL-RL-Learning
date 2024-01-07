9164a3c85a15bd9693301ccfb10bba4126984f07
yy <2572082773@qq.com>
2024-01-07 20:28:26 +0800

    update config

--- RL-Learning/GYM/config.py
+++ RL-Learning/GYM/config.py
@@ -1,6 +1,6 @@
-BATCH_SIZE = 128
-MAX_EPISODES = 10001
-MAX_STEPS = 5000
+BATCH_SIZE = 64
+MAX_EPISODES = 50000
+MAX_STEPS = 10000
 seed = 12345
 # state_dim, action_dim, hidden_dim, lr_actor, lr_critic, gamma
 lr_actor = 0.001
@@ -9,15 +9,19 @@ gamma = 0.99
 hidden_dim = 256
 # 设置加载点，加载模型
 load_points = 0
-# env_name = "ALE/DemonAttack-ram-v5"
+env_name = "ALE/DemonAttack-ram-v5"
+# env_name = "ALE/Pong-ram-v5"
+
 # env_name = "ALE/DemonAttack-v5"
-env_name = "ALE/DonkeyKong-v5"
+# env_name = "ALE/DonkeyKong-v5"
 # env = gym.make("ALE/DonkeyKong-v5",render_mode="rgb_array",obs_type="ram")
 
-target_update = 2
+target_update = 10
 
 n_updates_critic = 1
 m_updates_actor = 5
+
+
 
 hyperparameters = {
     "MAX_EPISODES": MAX_EPISODES,
@@ -31,5 +35,5 @@ hyperparameters = {
     "env_name":env_name,
     "target_update":target_update,
     "m_updates_actor":m_updates_actor,
-    "n_updates_critic":n_updates_critic
+    "n_updates_critic":n_updates_critic,
 }
--- RL-Learning/GYM/env.py
+++ RL-Learning/GYM/env.py
@@ -12,38 +12,39 @@ def episode_trigger_func(episode):
         return True
     return False
 
-# 去除env_name的/,取右边内容
-project_name = env_name.split('/')[-1]
+def make_env(env_name):
+
+    # 去除env_name的/,取右边内容
+    project_name = env_name.split('/')[-1]
+
+    # offline_directory="/path/to/save/experiments"
+    experiment = OfflineExperiment(
+    api_key="jxCKAgc1LK4bLO9pQIuerERSJ",
+    project_name=project_name,
+    workspace="gym",
+    offline_directory="./experiments"
+    )
+
+    # env = gym.make('Acrobot-v1', render_mode="rgb_array")
+    # 
+    env = gym.make(env_name,render_mode="rgb_array",obs_type="ram")
+    # env = FrameStack(env, 4) # 堆叠4帧，可以学习到场景的方向,此时维度是(4,128)
+    # print("STATE_DIM 2",env.observation_space.shape)
+    # env = FlattenObservation(env) # 转为1维的shape 
+    env = gym.wrappers.RecordVideo(env, f'video-{env_name}-{BATCH_SIZE}-{target_update}')
+    # env = CometLogger(env, experiment)
 
 
-# offline_directory="/path/to/save/experiments"
-experiment = OfflineExperiment(
-  api_key="jxCKAgc1LK4bLO9pQIuerERSJ",
-  project_name=project_name,
-  workspace="gym",
-  offline_directory="./experiments"
-)
+    STATE_DIM = env.observation_space.shape[0]
+    ACTION_DIM = env.action_space.n
+
+    hyperparameters['STATE_DIM'] = STATE_DIM
+    hyperparameters['ACTION_DIM'] = ACTION_DIM
 
 
+    print("env.observation_space",env.observation_space)
+    print("env.action_space",env.action_space)
+    # experiment.log_parameters(hyperparameters)    
+    return env
 
 
-# env = gym.make('Acrobot-v1', render_mode="rgb_array")
-# 
-env = gym.make(env_name,render_mode="rgb_array",obs_type="ram")
-# env = FrameStack(env, 4) # 堆叠4帧，可以学习到场景的方向,此时维度是(4,128)
-# print("STATE_DIM 2",env.observation_space.shape)
-# env = FlattenObservation(env) # 转为1维的shape 
-env = gym.wrappers.RecordVideo(env, f'video-{env_name}-{BATCH_SIZE}-{target_update}')
-env = CometLogger(env, experiment)
-
-
-STATE_DIM = env.observation_space.shape[0]
-ACTION_DIM = env.action_space.n
-
-hyperparameters['STATE_DIM'] = STATE_DIM
-hyperparameters['ACTION_DIM'] = ACTION_DIM
-
-
-print("env.observation_space",env.observation_space)
-print("env.action_space",env.action_space)
-# experiment.log_parameters(hyperparameters)
