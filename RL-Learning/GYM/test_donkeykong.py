09f475b869bb88c82cf6e0d062f2a2bc3a081dc3
yy <2572082773@qq.com>
2024-01-08 10:00:16 +0800

    update

--- RL-Learning/GYM/test_donkeykong.py
+++ RL-Learning/GYM/test_donkeykong.py
@@ -1,10 +1,23 @@
+# import gymnasium as gym
+# env = gym.make("ALE/DonkeyKong-v5", render_mode="human")
+# observation, info = env.reset(seed=42)
+# for _ in range(1000):
+#    action = env.action_space.sample()  # this is where you would insert your policy
+#    observation, reward, terminated, truncated, info = env.step(action)
+
+#    if terminated or truncated:
+#       observation, info = env.reset()
+
+# env.close()
+
 import gymnasium as gym
-env = gym.make("ALE/DonkeyKong-v5", render_mode="human")
+env = gym.make("ALE/DonkeyKong-v5", render_mode="human",obs_type="ram")
 observation, info = env.reset(seed=42)
-for _ in range(1000):
+for _ in range(100):
+   env.render()
    action = env.action_space.sample()  # this is where you would insert your policy
    observation, reward, terminated, truncated, info = env.step(action)
-
+   print(observation)
    if terminated or truncated:
       observation, info = env.reset()
 
